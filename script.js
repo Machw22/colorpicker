f02e41cc8dc7851fbb43d190a294209d479575a9
Matt Reams <reamsmatt15@gmail.com>
2022-09-19 22:55:49 -0500

    Created basic file structure.

---
+++ script.js
@@ -0,0 +1,21 @@
+//mreams1 colorpicker logic
+
+function EnterColor() {
+    
+    var rgbValue = "";
+    var convertValue ="";
+
+
+    rgbValue = prompt("Please enter a proper RGB value.");
+
+    while (rgbValue ===  "") {
+        prompt("Reminder, please enter a proper RGB value.")
+    }
+
+
+
+
+
+}
+
+EnterColor();
